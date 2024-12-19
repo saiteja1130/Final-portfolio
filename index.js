1c968fb41f23185324d180fbb08b9df30d23f0cf
saiteja1130 <saitejanetha1130@gmail.com>
2024-12-19 16:19:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-let menuCon=document.querySelector(".nav-list");
-let menuOpen=document.querySelector("#open-menu");
-let menuClose=document.querySelector("#close-menu");
+let navList=document.querySelector(".navbar-nav");
+let navItem=document.querySelectorAll(".nav-item");
+let navLink=document.querySelectorAll(".nav-link");
+let homeCon=document.querySelector(".name");
+let aboutmeCon=document.querySelector(".about-me");
+let mainCon=document.querySelector(".main");
+let skillsSet=document.querySelector(".skills");
+ //navItem queryselectorAll returns a node of classes list with the same name
+ //loop through each node using foreach method
+ //add an element to catch the every element(navActive)
+ //add eventlistener for the particular element that we catched ny the foreach loop(navActive)
+ //remove active class from the navItem using foreach Loop add remove for the Particular element
+ //now add new class to the node using navActive element
+navItem.forEach((navActive)=>{
+    navActive.addEventListener("click",()=>{
+        navItem.forEach(navActiveItem=>navActiveItem.classList.remove("active"))
+        navActive.classList.add("active");
+    })
+})
+
+navItem.forEach(navMenu=>{
+  
+    navMenu.addEventListener("click",()=>{
+        if(navMenu.innerText=="Home"){
+            homeCon.classList.add("visible");
+            homeCon.classList.remove("hidden");
+            aboutmeCon.classList.add("hidden");
+            aboutmeCon.classList.remove("visible");
+            skillsSet.classList.add("hidden");
+            skillsSet.classList.remove("visible-grid");
+        }
+        else if(navMenu.innerText=="About Me"){
+            homeCon.classList.remove("visible");
+            homeCon.classList.add("hidden");
+            aboutmeCon.classList.add("visible");
+            aboutmeCon.classList.remove("hidden");
+            skillsSet.classList.add("hidden");
+            skillsSet.classList.remove("visible-grid");
+        }
+        else if(navMenu.innerText=="Skills"){
+            homeCon.classList.remove("visible");
+            homeCon.classList.add("hidden");
+            aboutmeCon.classList.add("hidden");
+            aboutmeCon.classList.remove("visible");
+            skillsSet.classList.add("visible-grid");
+            skillsSet.classList.remove("hidden");
+        }
+        
+    })
+})
 
 
-menuOpen.addEventListener("click",()=>{
-    menuCon.style.transform= "translateY(0)";
-    menuClose.style.display="block";
-    menuOpen.style.display="none";
-})
-menuClose.addEventListener("click",()=>{
-    menuCon.style.transform= "translateY(-115%)";
-    menuClose.style.display="none";
-    menuOpen.style.display="block";
-})
+
